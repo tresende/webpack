ca916c30e462103ab7e6d4da54d799a4fd302721
Thiago Resende <thiago.gcresende@gmail.com>
2018-03-13 17:04:44 -0300

    Css e dev server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,40 @@
+//relative path
 const path = require('path');
+//uglify
+const babiliPlugin = require('babili-webpack-plugin');
+//extract css to separed file
+const extractTextPlugin = require('extract-text-webpack-plugin');
+//css minify
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+let plugins = [];
+
+plugins.push(
+    new extractTextPlugin("styles.css")
+);
+
+if (process.env.NODE_ENV == 'production') {
+    plugins.push(new babiliPlugin());
+    plugins.push(new optimizeCSSAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: { 
+            discardComments: {
+                removeAll: true 
+            }
+        },
+        canPrint: true
+     }));    
+}
 
 module.exports = {
     entry: './app-src/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
     },
     module: {
+        //file per file
         rules: [
             {
                 test: /\.js$/,
@@ -14,7 +42,31 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                use: extractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
             }
         ]
-    }
+    },
+    plugins: plugins
 }
